a6ef7332539376a18c6ce66a9a1407d38fb07507
SEONG Park <b_ack2@naver.com>
2021-12-26 17:23:12 +0900

    잔버그 해결
    버그 제거 목적으로 시바코인은 제거되었습니다.

--- index.js
+++ index.js
@@ -9,15 +9,17 @@ client.on('ready', () => {
 const UpdateTime = 5000;
 
 //기본 지급 돈
-let wallet = 1000;
+let wallet = 3000;
 
 //캐캐오 주가변경 알고리즘
-const kakao = 800;
 let kakaMove = 0;
 let kakaoJusik = '주가 정보 불러오기에 실패하였습니다.';
 let kakaoVa = '주가 정보 불러오기에 실패했습니다.';
 let kakaoIfDownOrUp = 0;
 let UpOrDownMessage = '주가 정보 불러오기에 실패했습니다.';
+
+//캐캐오 기본가격
+const kakao = 800;
 
 //캐캐오 구매 저장
 let myjusik_1 = undefined;
@@ -28,7 +30,7 @@ let MyHave = [''];
 //구매감지
 let sell = undefined;
 
-while (true){
+//while (true){
 setTimeout(function () {
 	//캐캐오 랜덤값 설정
 	kakaoMove = Math.floor(Math.random() * 20) + 8;
@@ -58,21 +60,59 @@ setTimeout(function () {
 	kakaoVa = kakaoJusik - kakao;
 	console.log('kakaoVa : ' + kakaoVa);
 }, UpdateTime);
-}
 
 client.on('messageCreate', (message) => {
 	//시세확인
 	if (message.content === '이코야 현재시세') {
 		message.channel.send(
-			'```캐캐오 : ' + kakaoJusik + '이코 (' + UpOrDownMessage + kakaoVa + '이코)```'
+			'```캐캐오 : ' +
+				kakaoJusik +
+				'이코 (' +
+				UpOrDownMessage +
+				kakaoVa +
+				'이코)```'
 		);
 	}
 
-	//구매의사 재확인
-	if (message.content === '이코야 구매 확인 캐캐오') {
-		console.log('정상 입력');
+	//지갑
+	if (message.content === '이코야 지갑') {
+		message.channel.send(
+			'현재 보유중인 돈은 총 ' +
+				wallet +
+				'이코이고\n현재 보유하고 있는 주식은 ' +
+				MyHave +
+				' 입니다.'
+		);
+	}
 
-		//구매의사 저장
+
+	//판매확인 캐캐오
+	if (message.content === '이코야 판매 확인 캐캐오') {
+		myjusik_1 = 0;
+		wallet = wallet + kakaoJusik;
+		MyHave.splice('캐캐오');
+		message.channel.send(
+			'지갑에 ' + kakaoJusik + '이코가 입금되었습니다.\n현재 ' + wallet + '이코 보유중입니다.'
+		);
+	}
+	
+	//주식구매 캐캐오
+	if (message.content === '이코야 구매 캐캐오') {
+			if (myjusik_1 == '캐캐오') {
+				message.channel.send('이미 캐캐오 주식을 보유하고 있습니다.');
+			}
+			else {
+				message.channel.send('캐캐오의 현재 시세는 ' + kakaoJusik + '이코입니다.\n정말로 구매하시겠습니까?\n구매하시려면 **"이코야 구매 확인 캐캐오"**을 입력해주세요.');
+			}
+	}
+
+	//구매의사 재확인
+	if (message.content === '이코야 구매 확인 캐캐오') {console.log('정상 입력');
+		if (myjusik_1 == '캐캐오') {
+			message.channel.send('이미 캐캐오 주식을 보유하고 있습니다.');
+		}
+		else {
+			//구매의사 저장
 		buy = '구매완료';
 		console.log(sell);
 
@@ -97,6 +137,14 @@ client.on('messageCreate', (message) => {
 				message.channel.send('지갑에 돈이 부족하여 구매가 불가능합니다.');
 			}
 		}
+		}
+	}
+
+	//도움말
+	if (message.content === '이코야') {
+		message.channel.send(
+			'```이코봇 도움말\n \n이코야 현재시세 : 지금 주식들의 시세를 표시합니다.\n이코야 구매 [주식명] : 주식을 구매합니다.\n이코야 구매 확인 [주식명] : 주식을 확인 절차 없이 바로 구매하니 조심해서 사용해주세요!\n이코야 판매 [주식명] : 주식을 판매합니다.\n이코야 판매 확인 [주식명] : 주식을 확인 절차 없이 바로 판매하니 조심해서 사용해주세요!\n이코야 지갑 : 나의 보유 주식과 보유한 돈을 확인합니다.\n이코야 : 이코봇에 대한 도움말을 표시합니다.```'
+		);
 	}
 
 	//판매
@@ -112,47 +160,9 @@ client.on('messageCreate', (message) => {
 		}
 	}
 
-	if (message.content === '이코야 판매 확인 캐캐오') {
-		myjusik_1 = 0;
-		wallet = wallet + kakaoJusik;
-		MyHave.splice(1);
-		message.channel.send(
-			'지갑에 ' + kakaoJusik + '이코가 입금되었습니다.\n현재 ' + wallet + '이코 보유중입니다.'
-		);
-	}
-
-	//지갑
-	if (message.content === '이코야 지갑') {
-		message.channel.send(
-			'현재 보유중인 돈은 총 ' +
-				wallet +
-				'이코이고\n현재 보유하고 있는 주식은 ' +
-				MyHave +
-				' 입니다.'
-		);
-	}
-	
-	//주식구매
-	if (message.content === '이코야 구매 캐캐오') {
-		if (myjusik_1 == '캐캐오') {
-			message.channel.send('이미 구매를 했습니다.');
-		} else {
-			message.channel.send(
-				'캐캐오의 현재 시세는 ' +
-					kakaoJusik +
-					'이코입니다.\n정말로 구매하시겠습니까?\n구매하시려면 **"이코야 구매 확인 캐캐오"**을 입력해주세요.'
-			);
-		}
-	}
-	
-	//주식구매
-	if (message.content === '이코야') {
-		message.channel.send('```이코봇 도움말\n \n이코야 현재시세 : 지금 주식들의 시세를 표시합니다.\n이코야 구매 [주식명] : 주식을 구매합니다.\n이코야 구매 확인 [주식명] : 주식을 확인 절차 없이 바로 구매하니 조심해서 사용해주세요!\n이코야 판매 [주식명] : 주식을 판매합니다.\n이코야 판매 확인 [주식명] : 주식을 확인 절차 없이 바로 판매하니 조심해서 사용해주세요!\n이코야 지갑 : 나의 보유 주식과 보유한 돈을 확인합니다.\n이코야 : 이코봇에 대한 도움말을 표시합니다.```');
-	}
-	
 	if (!message.guild) return;
 	if (message.author.bot) return;
 });
 
 // 디스코드 클라이언트 토큰 (유출금지 ㄹㅇ찐임)
-client.login('token');
+client.login('windows'); 
